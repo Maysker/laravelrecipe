1d001632fbadad81a3597466be121ac0d6a203fe
Adam Gazdiev <a.gazdiev@gmail.com>
2024-01-27 18:48:26 +0100

    Creating Routes for Authentication

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/users', [UserController::class, 'index']);
 Route::post('/users', [UserController::class, 'store']);
@@ -13,6 +14,15 @@ Route::delete('/users/{id}', [UserController::class, 'destroy']);
 
 Route::get('/recipes', [RecipeController::class, 'index']);
 Route::post('/recipes', [RecipeController::class, 'store']);
+
+// Routes for registration and authentication
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Routes that require authentication
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
